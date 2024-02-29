54a41208cc42f48216a3b933397ae6d1c2cc2e83
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-02-29 15:21:52 +0530

    add image

--- src/CarouselPage.jsx
+++ src/CarouselPage.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
 import './CarouselPage.css'
+import image from  "./assets/w1.jpg"
 
 
 function CarouselPage() {
   return (
-
-
-    <div className='container '>
-      
-      <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-        <div class="col-lg-6 px-0">
-          <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Continue reading...</a></p>
-        </div>
-      </div>
-
-      </div>
-
+    <img src={image} class="img-fluid img" alt="Responsive image"/>
+          
   )
 }
 
